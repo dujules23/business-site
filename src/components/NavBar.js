783786362a0391b356cdce6dfb8f41dd89231d60
Durrell Jules <71855759+dujules23@users.noreply.github.com>
2022-09-20 17:55:21 -0400

    began working on nav bar responsiveness, need to fix nav bar link styling

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,10 +1,22 @@
+import React, { useState } from "react";
 import { MenuData } from "../data/MenuData";
+import "../styles/_navbar.scss";
 
 export default function NavBar() {
+  const [clicked, setClicked] = useState(false);
+
+  // handle click function
+  const handleClick = () => {
+    setClicked(!clicked);
+  };
+
   return (
-    <nav>
-      <h1>Q and Sons</h1>
-      <ul>
+    <nav className="nav-bar-items">
+      <h1 className="logo">Q and Sons</h1>
+      <div className="menu-icons" onClick={handleClick}>
+        <i className={clicked ? "fas fa-times" : "fas fa-bars"}></i>
+      </div>
+      <ul className="nav-menu">
         {MenuData.map((item, index) => {
           return (
             <li key={index} className={item.cName}>
