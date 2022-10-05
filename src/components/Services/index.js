56d2f0dd96fc18746ab7ca4cf42f605e3e4c0bfc
Durrell Jules <71855759+dujules23@users.noreply.github.com>
2022-10-05 13:27:18 -0400

    refactored Services, abstracted data into an array for cleaning up code

--- src/components/Services/index.js
+++ src/components/Services/index.js
@@ -1,7 +1,8 @@
 import React from "react";
-import Icon1 from "../../images/svg-2.svg";
-import Icon2 from "../../images/svg-3.svg";
-import Icon3 from "../../images/svg-4.svg";
+// import Icon1 from "../../images/svg-2.svg";
+// import Icon2 from "../../images/svg-3.svg";
+// import Icon3 from "../../images/svg-4.svg";
+import { ServicesData } from "../../data/ServicesData";
 import {
   ServicesContainer,
   ServicesH1,
@@ -17,36 +18,15 @@ const Services = () => {
     <ServicesContainer id="services">
       <ServicesH1>Our Services</ServicesH1>
       <ServicesWrapper>
-        <ServicesCard>
-          <ServicesIcon src={Icon1} />
-          <ServicesH2>Basic Cleaning</ServicesH2>
-          <ServicesP>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat.
-          </ServicesP>
-        </ServicesCard>
-        <ServicesCard>
-          <ServicesIcon src={Icon3} />
-          <ServicesH2>Deep Cleaning</ServicesH2>
-          <ServicesP>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat.
-          </ServicesP>
-        </ServicesCard>
-        <ServicesCard>
-          <ServicesIcon src={Icon2} />
-          <ServicesH2>Commercial</ServicesH2>
-          <ServicesP>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat.
-          </ServicesP>
-        </ServicesCard>
+        {ServicesData.map((item, index) => {
+          return (
+            <ServicesCard key={index}>
+              <ServicesIcon src={item.picture} />
+              <ServicesH2>{item.title}</ServicesH2>
+              <ServicesP>{item.description}</ServicesP>
+            </ServicesCard>
+          );
+        })}
       </ServicesWrapper>
     </ServicesContainer>
   );
---
+++ src/data/ServicesData.js
@@ -0,0 +1,30 @@
+import Icon1 from "../images/svg-2.svg";
+import Icon2 from "../images/svg-3.svg";
+import Icon3 from "../images/svg-4.svg";
+
+export const ServicesData = [
+  {
+    picture: Icon1,
+    title: "Basic Cleaning",
+    description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+    eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+    ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+    aliquip ex ea commodo consequat.`,
+  },
+  {
+    picture: Icon2,
+    title: "Deep Cleaning",
+    description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+    eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+    ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+    aliquip ex ea commodo consequat.`,
+  },
+  {
+    picture: Icon3,
+    title: "Commercial",
+    description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+    eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+    ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+    aliquip ex ea commodo consequat.`,
+  },
+];
