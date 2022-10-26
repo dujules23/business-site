b80e0d0d61022dca78b733462616a6f566cec923
Durrell Jules <71855759+dujules23@users.noreply.github.com>
2022-10-25 22:22:06 -0400

    having issues with map location

--- src/components/Map/index.js
+++ src/components/Map/index.js
@@ -1,21 +1,34 @@
 import React from "react";
 import { MapInfo, MapWrapper } from "./MapElements";
-import { MapContainer, TileLayer, Marker } from "react-leaflet";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import "../Map/map.scss";
+
 const Map = () => {
   return (
     <>
-      <MapInfo>
+      <div className="info-map">
         {`Q & Sons `}
         <br />
         Port Wentworth, Georiga <br />
         <span>email@email.com</span>
-      </MapInfo>
-      <MapWrapper>
+      </div>
+      <div className="map-wrap">
         <MapContainer center={[44.96366, 19.61045]} zoom={13}>
           <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
           <Marker position={[44.96366, 19.61045]}></Marker>
         </MapContainer>
-      </MapWrapper>
+      </div>
+      {/* <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <Marker position={[51.505, -0.09]}>
+          <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable.
+          </Popup>
+        </Marker>
+      </MapContainer> */}
     </>
   );
 };
